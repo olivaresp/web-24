baf7bfb25f7c90f2440e5402dd95eaccfac78535
olivaresp <olivares.pg@gmail.com>
2024-09-04 15:49:50 -0300

    integración analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
+import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
+ 
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true,
+    },
   }),
 });
