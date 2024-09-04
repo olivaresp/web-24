860e16637934e148f35ad54f5f9ae030b917a78f
olivaresp <olivares.pg@gmail.com>
2024-09-04 01:20:11 -0300

    añade más información al portafolio

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -17,10 +17,27 @@ export const ui = {
     'footer.rights': "All rights reserved",
 
     // Portfolio
-    'work.ilegal.title': "Custom WordPress Theme Development - Fiesta Ilegal (2024)",
-    'work.academia.title': "Online Learning Platform Development - Academia Maker (2024)",
-    'work.vina.title': "Website Development - Viña del Mar Festival (2024)",
-    'work.atix.title': "Academic Management Platform Development - Atix (2015)",
+    'work.role': "ROLE",
+
+    'work.ilegal.year': "2024",
+    'work.ilegal.title': "Custom WordPress Theme Development - Fiesta Ilegal",
+    'work.ilegal.role': "Full-Stack Developer and UX/UI Designer",
+    'work.ilegal.description': "Designed and developed a custom WordPress theme for Fiesta Ilegal, a platform dedicated to promoting underground Chilean music. The project involved close collaboration with the client to analyze requirements and specifications. My role encompassed designing the user interface in Figma, developing the theme using WordPress, configuring the platform for optimal performance, and providing ongoing maintenance. The site features sections for news, new releases, and DJ sets, tailored to the needs of the underground music community.",
+
+    'work.academia.year': "2024",
+    'work.academia.title': "Online Learning Platform Development - Academia Maker",
+    'work.academia.role': "Front-End Developer and UX/UI Designer",
+    'work.academia.description': "Designed and developed the online learning platform 'Academia Maker', where professors from four public universities in Chile can upload and manage their courses. Students can enroll and complete courses online through a user-friendly interface. The platform includes a comprehensive management system for administrators, professors, and students, as well as a resource repository and a community forum section. The development involved Vue.js for the front-end, Node.js for the back-end, Google services for authentication, and Heroku for hosting and operational management.",
+
+    'work.vina.year': "2024",
+    'work.vina.title': "Website Development - Viña del Mar Festival",
+    'work.vina.role': "Full-Stack Developer and UX/UI Designer",
+    'work.vina.description': "Collaboratively designed and developed the official website for the prestigious Viña del Mar Festival 2024 in Chile, in partnership with the national public television channel TVN and the private channel C13. The website's primary objective was to facilitate the sale of advertising slots, both for the live broadcast by the television channels and the physical spaces in Viña del Mar. The site provided detailed information and requirements for companies to book advertising spaces. Hosted on Vercel, the website also featured SEO optimizations to maximize visibility and reach.",
+
+    'work.atix.year': "2015",
+    'work.atix.title': "Academic Management Platform Development - Atix",
+    'work.atix.role': "Front-End Developer and UX/UI Designer",
+    'work.atix.description': "Developed an integrated platform for managing schools and educational institutions using Ruby on Rails, HTML, CSS, and JavaScript. Includes features for academic administration, student tracking, and communication between parents and teachers.",
 
     // Connect
     'connect.title': "Be cool",
@@ -35,6 +52,24 @@ export const ui = {
     'thanks.title': "Thank you!",
     'thanks.description': "You will soon receive an email in your inbox. Let's keep in touch :)",
     'thanks.action': "Back home",
+    
+    // Tags
+    'tag.skills.front-end-development': "Front-End Development",
+    'tag.skills.ux-ui-Design': "UX & UI Design",
+    'tag.skills.javascript': "JavaScript",
+    'tag.skills.ruby-on-rails': "Ruby on Rails",
+    'tag.skills.devops': "DevOps",
+    'tag.skills.wordpress-development': "WordPress Development",
+    'tag.skills.figma': "Figma",
+    'tag.skills.web-maintenance': "Web Maintenance",
+    'tag.skills.seo-optimization': "SEO Optimization",
+    'tag.skills.vue-js-development': "Vue.js Development",
+    'tag.skills.node-js': "Node.js",
+    'tag.skills.google-services': "Google Services",
+    'tag.skills.heroku-deployment': "Heroku Deployment",
+    'tag.skills.astro-build-development': "Astro.build Development",
+    'tag.skills.vercel-hosting': "Vercel Hosting",
+    'tag.skills.information-architecture': "Information Architecture"
   },
   es: {
     // Nav
@@ -45,10 +80,19 @@ export const ui = {
     'footer.rights': "Todos los derechos reservados",
 
     // Portfolio
-    'work.ilegal.title': "Desarrollo Tema Personalizado WordPress - Fiesta Ilegal (2024)",
-    'work.academia.title': "Desarrollo Plataforma de Aprendizaje Online - Academia Maker (2024)",
-    'work.vina.title': "Desarrollo Sitio Web - Festival de Viña del Mar (2024)",
-    'work.atix.title': "Desarrollo Plataforma de Gestión Académica - Atix (2015)",
+    'work.role': "ROL",
+
+    'work.ilegal.title': "Desarrollo Tema Personalizado WordPress - Fiesta Ilegal",
+    'work.ilegal.description': "Diseñé y desarrollé un tema personalizado en WordPress para Fiesta Ilegal, una plataforma dedicada a la difusión de la música underground chilena. El proyecto implicó una estrecha colaboración con el cliente para analizar los requerimientos y especificaciones. Mi trabajo abarcó el diseño de la interfaz de usuario en Figma, el desarrollo del tema en WordPress, la configuración de la plataforma para un rendimiento óptimo y el mantenimiento continuo. El sitio incluye secciones para noticias, nuevos lanzamientos y DJ sets, adaptadas a las necesidades de la comunidad musical underground.",
+
+    'work.academia.title': "Desarrollo Plataforma de Aprendizaje Online - Academia Maker",
+    'work.academia.description': "Diseñé y desarrollé la plataforma de aprendizaje online 'Academia Maker', donde profesores de cuatro universidades públicas de Chile pueden cargar y gestionar sus cursos. Los estudiantes pueden inscribirse y rendir sus cursos online a través de una interfaz fácil de usar. La plataforma incluye un sistema de gestión completo para administradores, profesores y estudiantes, así como un repositorio de recursos y una sección de comunidad tipo foro. El desarrollo involucró Vue.js para el front-end, Node.js para el back-end, servicios de Google para autenticación, y Heroku para el alojamiento y la gestión operativa.",
+
+    'work.vina.title': "Desarrollo Sitio Web - Festival de Viña del Mar",
+    'work.vina.description': "Diseñé y desarrollé en colaboración con el cliente el sitio web oficial del prestigioso Festival de Viña del Mar 2024 en Chile, en conjunto con los canales de televisión TVN (público) y C13 (privado). El principal objetivo del sitio era facilitar la venta de espacios publicitarios, tanto para la transmisión en vivo por parte de los canales de televisión como para los espacios físicos en la ciudad de Viña del Mar. El sitio proporcionaba información detallada y los requisitos para que las empresas pudieran contratar estos espacios. El sitio fue alojado en Vercel e incluyó optimizaciones SEO para maximizar la visibilidad y el alcance.",
+
+    'work.atix.title': "Desarrollo Plataforma de Gestión Académica - Atix",
+    'work.atix.description': "Desarrollo de una plataforma integral para la gestión de colegios e instituciones educativas, utilizando Ruby on Rails, HTML, CSS y JavaScript. Incluye funciones de administración académica, seguimiento de estudiantes y comunicación entre padres y docentes.",
 
     // Connect
     'connect.title': "Tranquilo",
@@ -63,6 +107,24 @@ export const ui = {
     'thanks.title': "Gracias!",
     'thanks.description': "Pronto recibirás un correo electrónico en tu bandeja de entrada. Estamos en contacto :)",
     'thanks.action': "Volver al inicio",
+
+    // Tags
+    'tag.skills.front-end-development': "Desarrollo Front-End",
+    'tag.skills.ux-ui-Design': "Diseño UX y UI",
+    'tag.skills.javascript': "JavaScript",
+    'tag.skills.ruby-on-rails': "Ruby on Rails",
+    'tag.skills.devops': "DevOps",
+    'tag.skills.wordpress-development': "Desarrollo WordPress",
+    'tag.skills.figma': "Figma",
+    'tag.skills.web-maintenance': "Mantenimiento Web",
+    'tag.skills.seo-optimization': "Optimización SEO",
+    'tag.skills.vue-js-development': "Desarrollo Vue.js",
+    'tag.skills.node-js': "Node.js",
+    'tag.skills.google-services': "Servicios de Google",
+    'tag.skills.heroku-deployment': "Despliegue Heroku",
+    'tag.skills.astro-build-development': "Desarrollo Astro.build",
+    'tag.skills.vercel-hosting': "Despliegue Vercel",
+    'tag.skills.information-architecture': "Arqitectura de información"
   },
 } as const;
 
