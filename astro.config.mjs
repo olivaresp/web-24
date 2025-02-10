951ed1b464a509eabfd58ba853ca43e54d2fb5a1
Andrés <olivares.pg@gmail.com>
2025-02-10 19:57:37 -0300

    configura el server render

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
-  output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: { enabled: true },
   }),
 });
